31e37b667a8b5f6254523490314b52698abf2e73
Daryl494 <75987264+Daryl494@users.noreply.github.com>
2020-12-15 16:26:12 +0000

    Update TodayFragment.java

--- app/src/main/java/com/zzs/timemaster/Tabs/TodayFragment.java
+++ app/src/main/java/com/zzs/timemaster/Tabs/TodayFragment.java
@@ -1,32 +1,77 @@
 package com.zzs.timemaster.Tabs;
 
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.sqllite.MyOpenHelper;
 import com.zzs.timemaster.Models.Arrangement;
+import com.zzs.timemaster.Models.Event;
 import com.zzs.timemaster.R;
 import com.zzs.timemaster.databinding.FragmentTodayBinding;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class TodayFragment extends Fragment {
 
     FragmentTodayBinding binding;
 
     ArrayList<Arrangement> schedules = new ArrayList<>();
+    private Context nContext;
+    TextView time2;
 
     public TodayFragment() {
     }
+    public ArrayList<Event> getData() {
+        ArrayList<Event> events=new ArrayList<>();
+        MyOpenHelper myHelper = new MyOpenHelper(getContext(), MyOpenHelper.DB_NAME, null, 1);
+        SQLiteDatabase db = myHelper.getWritableDatabase();
+        Cursor c = db.query(MyOpenHelper.TABLE_NAME, new String[] {
+                        MyOpenHelper.EVENT_NAME, MyOpenHelper.EVENT_LOCATION,MyOpenHelper.TIME, }, null,null,
+                null, null, null);
+        int nameIndex=c.getColumnIndex(MyOpenHelper.EVENT_NAME);
+        int locationIndex=c.getColumnIndex(MyOpenHelper.EVENT_LOCATION);
+        int timeIndex=c.getColumnIndex(MyOpenHelper.TIME);
 
+        while(c.moveToNext()){
+            String name = c.getString(nameIndex);
+            String location = c.getString(locationIndex);
+            String time=c.getString(timeIndex);
+            Event event=new Event(time,name,location);
+            events.add(event);
+        }
+        db.close();
+        return events;
+    }
+
+    public void loadArrangement(){
+        ArrayList<Event>events=getData();
+        SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyyMMdd");
+        String date=simpleDateFormat.format(new Date());
+        String []Words={"一","二","三","四","五","六","七","八","九","十"};
+        int count=0;
+        for(Event event: events){
+            if(event.getEvent_time().equals(date)){
+                schedules.add(new Arrangement("今日事项"+Words[count]+": ", event.getEvent_name(), event.getEvent_location()));
+                count++;}
+
+        }
+    }
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,9 +82,8 @@ public class TodayFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        schedules.add(new Arrangement("10:10 - 11:30", "口译（1）", "北六教A311"));
-        schedules.add(new Arrangement("10:10 - 11:30", "口译（1）", "北六教A311"));
+        //获取数据
+        loadArrangement();
 
         binding.TodayList.setLayoutManager(new LinearLayoutManager(getContext()));
         binding.TodayList.setAdapter(new TodayFragment.TodayListAdapter(getContext(), schedules));
@@ -88,5 +132,21 @@ public class TodayFragment extends Fragment {
                 subtitle = itemView.findViewById(R.id.TodayItemSubtitle);
             }
         }
+    }
+
+    @Override
+    public void onResume() {
+        this.nContext=getActivity();
+        super.onResume();
+        FutureFragment futureFragment=new FutureFragment();
+        boolean eventChange=futureFragment.isChanged;
+        futureFragment.isChanged=false;
+        if(eventChange){
+            schedules.clear();
+            loadArrangement();
+            time2=getView().findViewById(R.id.TodayItemTitle);
+            time2.setText(time2.getText().toString());
+//        Toast.makeText(nContext,"已更新",Toast.LENGTH_SHORT).show();
+            }
     }
 }
